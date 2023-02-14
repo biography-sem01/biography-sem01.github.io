6d8a4234c1306fa8766df5c78ed9cd38beb86a0e
biography-sem01 <project1@hdweb.vn>
2023-02-14 21:15:05 +0700

    D3-Go Home

--- js/myController.js
+++ js/myController.js
@@ -4,9 +4,11 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", { templateUrl: "./views/home.html" })
-        .when("/news", { templateUrl: "./views/news.html" })
+        .when("/news", { templateUrl: "./views/news.html", controller: 'pageNews' })
         .when("/news/:newsId", { templateUrl: "./views/news-detail.html", controller: 'newDetailController' })
-        .when("/nobel-prizes", { templateUrl: "./views/detail-biography.html" })
+        .when("/nobel-prizes", { templateUrl: "./views/nobel-prizes.html", controller: 'pageNobelList' })
+        .when("/nobel-prizes/:bioId", { templateUrl: "./views/detail-bio.html", controller: 'detailBio' })
+        .when("/bio", { templateUrl: "./views/detail-biography.html" })
         .otherwise({ redirectTo: '/' })
     // $locationProvider.html5Mode(true);
 });
@@ -23,10 +25,10 @@ app.controller('pageHome', function ($scope, $http, $sce) {
         });
     });
 });
-
-app.controller('pagePrizes', function ($scope, $http) {
-
-
+app.controller('pageNobelList', function ($scope, $http) {
+    $http.get("./data/nobellist.json").then(function (resjson) {
+        $scope.nobellist = resjson.data.nobellist;
+    });
 });
 app.controller('pageNews', function ($scope, $http) {
     $http.get("./data/news.json").then(function (resjson) {
@@ -41,11 +43,4 @@ app.controller('newDetailController', function ($scope, $http, $routeParams) {
         $scope.fullnews = $scope.detailnews.find(function (item) { return item.link === link; });
     });
 
-});
-
-angular.module('myApp').component('greetUser', {
-    template: 'Hello, {{$ctrl.user}}!',
-    controller: function GreetUserController() {
-        this.user = 'world';
-    }
 });
