043e0f929d8b44697b783c6560cef61bfd9a070f
biography-sem01 <project1@hdweb.vn>
2023-02-14 14:34:10 +0700

    D3-OMG i just learn routeParams - So hard without a guide

--- js/myController.js
+++ js/myController.js
@@ -4,7 +4,9 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", { templateUrl: "./views/home.html" })
-        .when("/bio", { templateUrl: "./views/detail-biography.html" })
+        .when("/news", { templateUrl: "./views/news.html" })
+        .when("/news/:newsId", { templateUrl: "./views/news-detail.html", controller: 'newDetailController' })
+        .when("/nobel-prizes", { templateUrl: "./views/detail-biography.html" })
         .otherwise({ redirectTo: '/' })
     // $locationProvider.html5Mode(true);
 });
@@ -29,7 +31,21 @@ app.controller('pagePrizes', function ($scope, $http) {
 app.controller('pageNews', function ($scope, $http) {
     $http.get("./data/news.json").then(function (resjson) {
         $scope.news = resjson.data.news;
-        console.log($scope.fnews);
     });
 
+});
+app.controller('newDetailController', function ($scope, $http, $routeParams) {
+    $http.get("./data/news.json").then(function (resjson) {
+        $scope.detailnews = resjson.data.news;
+        var link = $routeParams.newsId;
+        $scope.fullnews = $scope.detailnews.find(function (item) { return item.link === link; });
+    });
+
+});
+
+angular.module('myApp').component('greetUser', {
+    template: 'Hello, {{$ctrl.user}}!',
+    controller: function GreetUserController() {
+        this.user = 'world';
+    }
 });
